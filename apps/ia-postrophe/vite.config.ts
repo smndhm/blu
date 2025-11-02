7a3512737860768e5fc4b3b24a94368b66403325
smndhm <simon.duhem@gmail.com>
2025-11-02 18:21:58 +0100

    chore: update build

--- apps/ia-postrophe/vite.config.ts
+++ apps/ia-postrophe/vite.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/blu/ia-postrophe/' : '/',
+  build: {
+    outDir: 'dist/ia-postrophe',
+  },
 }));
